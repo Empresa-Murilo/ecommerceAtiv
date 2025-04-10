5d0a57e0637d323ad8e13adc2b1a0e7d61396e47
Murilo <Murisuzuki11@gmail.com>
2025-04-09 21:09:04 -0300

    Criação da Tabela Clientes

--- script.sql
+++ script.sql
@@ -10,11 +10,25 @@ CREATE TABLE Usuario(
 );
 
 CREATE TABLE Cliente(
-	CodCli int primary key auto_increment,
-    NomeCli varchar(50) not null,
-    TelCli varchar(20) not null,
-    EmailCli varchar(50) not null
-)
+		CodCli int primary key auto_increment,
+        NomeCli varchar(50) not null,
+        TeleCli varchar(20) not null,
+        EmailCli varchar(50) not null
+);
+
+CREATE TABLE Produto(
+		CodProd int primary key auto_increment,
+        /*Chave estrangeira*/
+        CodCli int,
+        NomeProd varchar(50),
+        DescProd varchar(60),
+        QuantProd int,
+        PrecoProd int,
+        foreign key (CodCli) references Cliente (CodCli)
+);
 
 SELECT * FROM Usuario;
 SELECT * FROM Cliente;
+SELECT * FROM Produto;
+
+insert into Usuario(Nome, Email, Senha)VALUES('admin','admin@gmail.com','12345678');
